9988dce5314dba5ba8e24add6e9e5d9acc34cef7
Guyutongxue <guyutongxue@163.com>
2025-01-04 12:52:19 +0800

    fix internal server err

--- packages/config/main.ts
+++ packages/config/main.ts
@@ -1,7 +1,7 @@
 /** Toggle this when editing beta version. */
 export const IS_BETA = false;
 
-export const BETA_VERSION = "v9999.beta";
+export const BETA_VERSION = "v9999.0.0.beta";
 
 export const DEFAULT_ASSET_API_ENDPOINT = IS_BETA
   ? `https://beta.assets.gi-tcg.guyutongxue.site/api/v2`
